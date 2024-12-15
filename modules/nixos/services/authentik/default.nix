451a6588af2bbb5cdea336cf7d7559cfd25d5c89
Thomas <tlux14@googlemail.com>
2024-12-15 16:37:36 +0100

    debug

--- modules/nixos/services/authentik/default.nix
+++ modules/nixos/services/authentik/default.nix
@@ -8,31 +8,31 @@ with lib;
 with lib.luxnix; let
   cfg = config.services.luxnix.authentik;
 in {
-  options.services.luxnix.authentik = with types; {
-    enable = mkBoolOpt false "Enable authentik host";
-  };
+  # options.services.luxnix.authentik = with types; {
+  #   enable = mkBoolOpt false "Enable authentik host";
+  # };
 
-  config = mkIf cfg.enable {
-    # for prototyping
-    environment.etc."authentik/env".text = ''
-      authenik_env: |
-        AUTHENTIK_SECRET_KEY=asdasd
-        AUTHENTIK_EMAIL__PASSWORD=asdasd
-      '';
+  # config = mkIf cfg.enable {
+  #   # for prototyping
+  #   environment.etc."authentik/env".text = ''
+  #     authenik_env: |
+  #       AUTHENTIK_SECRET_KEY=asdasd
+  #       AUTHENTIK_EMAIL__PASSWORD=asdasd
+  #     '';
 
-    services = {
-      authentik = {
-        enable = true;
-        environmentFile = "/etc/authentik/env";
-        settings = {
-          email = {
-          # email settings
-          };
-          disable_startup_analytics = true;
-          avatars = "initials";
-        };
-      };
-    };
+  #   services = {
+  #     authentik = {
+  #       enable = true;
+  #       environmentFile = "/etc/authentik/env";
+  #       settings = {
+  #         email = {
+  #         # email settings
+  #         };
+  #         disable_startup_analytics = true;
+  #         avatars = "initials";
+  #       };
+  #     };
+  #   };
 
-  };
+  # };
 }
