89d39c949875fe486a00692496031e19f552548d
Thomas <tlux14@googlemail.com>
2024-12-16 18:30:46 +0100

    git change reverterted wierd user management errors happened

--- modules/home/cli/programs/git/default.nix
+++ modules/home/cli/programs/git/default.nix
@@ -16,6 +16,8 @@ with lib.luxnix; let
 in {
   options.cli.programs.git = with types; {
     enable = mkBoolOpt false "Whether or not to enable git.";
+    userName = mkOpt str "Thomas" "The name to use with git.";
+    email = mkOpt (nullOr str) "tlux14@googlemail.com" "The email to use with git.";
     urlRewrites = mkOpt (attrsOf str) {} "url we need to rewrite i.e. ssh to http";
     allowedSigners = mkOpt str "" "The public key used for signing commits";
   };
@@ -30,11 +32,6 @@ in {
 
       extraConfig =
         {
-          credential = {
-            helper = "store --file ~/.git-credentials";
-          };
-          credential.modalPrompt = true;
-
           # gpg.format = "ssh";
           # gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
           # commit.gpgsign = true;
@@ -43,7 +40,7 @@ in {
           # core = {
           #   editor = "nvim";
           #   pager = "delta";
-          # };{}
+          # };
 
           # color = {
           #   ui = true;
