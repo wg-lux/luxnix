84a052d93e9a1a7c20d6726072439ea3e6d47a4d
Thomas <tlux14@googlemail.com>
2024-12-19 14:38:20 +0100

    add gc-09

---
+++ homes/x86_64-linux/admin@gc-09/default.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  # cli.programs.git.allowedSigners = ; #TODO
+
+  desktops = {
+    plasma = {
+      enable = true;
+    };
+  };
+
+  services.luxnix = {
+    # syncthing.enable = false;
+  };
+
+  roles = {
+    development.enable = true;
+    social.enable = true;
+    gpu.enable = true;
+    video.enable = true;
+  };
+
+  home.stateVersion = "23.11";
+}
