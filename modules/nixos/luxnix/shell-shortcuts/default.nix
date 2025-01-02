7e1e64f1bb59b9b1c5d58009b4c423c04b87f0d2
maddonix <tlux14@googlemail.com>
2025-01-02 16:19:57 +0100

    rm update TOC

--- modules/nixos/luxnix/shell-shortcuts/default.nix
+++ modules/nixos/luxnix/shell-shortcuts/default.nix
@@ -1,11 +1,12 @@
 { config, lib, pkgs, ... }:
 with lib;
 {
-  system.activationScripts.updateToc = {
-    deps = [];
-    text = ''
-      echo "Updating Table of Contents..."
-      ${pkgs.python3}/bin/python3 ${./shell-shortcuts.py}
-    '';
-  };
+  #FIXME @maxhild
+  # system.activationScripts.updateToc = {
+  #   deps = [];
+  #   text = ''
+  #     echo "Updating Table of Contents..."
+  #     ${pkgs.python3}/bin/python3 ${./shell-shortcuts.py}
+  #   '';
+  # };
 }
