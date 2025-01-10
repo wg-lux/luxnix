4ba48eeeeb387a3e820cf30d01f4bab271606772
maddonix <tlux14@googlemail.com>
2025-01-10 19:16:42 +0100

    autoconf testing

--- systems/x86_64-linux/s-02/default.nix
+++ systems/x86_64-linux/s-02/default.nix
@@ -9,14 +9,21 @@
   ];
 
   user = {
-    admin = {
-      name = "admin";
+    admin= {
+    name= "admin";
     };
     ansible.enable = true;
     settings.mutable = false;
   };
 
-  roles = { base-server.enable = true; .enable = true; };
+roles = {
+    base-server.enable= true;
+    aglnet.client.enable= true;
+    ssh-access.dev-01.enable= true;
+    ssh-access.dev-01.idEd25519= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh2Bg+mSSvA80ALScpb81Q9ZaBFdacdxJZtAfZpwYkK;
+    };
 
-  services = { };
+  services = {
+
+};
 }
