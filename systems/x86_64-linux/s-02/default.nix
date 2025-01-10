29edb5994c4f60c421d765c6395fb872fe04f219
maddonix <tlux14@googlemail.com>
2025-01-10 19:26:17 +0100

    fix

--- systems/x86_64-linux/s-02/default.nix
+++ systems/x86_64-linux/s-02/default.nix
@@ -20,7 +20,7 @@ roles = {
     base-server.enable= true;
     aglnet.client.enable= true;
     ssh-access.dev-01.enable= true;
-    ssh-access.dev-01.idEd25519= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh2Bg+mSSvA80ALScpb81Q9ZaBFdacdxJZtAfZpwYkK;
+    ssh-access.dev-01.idEd25519= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh2Bg+mSSvA80ALScpb81Q9ZaBFdacdxJZtAfZpwYkK";
     };
 
   services = {
