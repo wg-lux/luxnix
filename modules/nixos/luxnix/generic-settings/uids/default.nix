9a863440f603dd8135e78fd364784f5fdfa84305
maddonix <tlux14@googlemail.com>
2025-01-14 09:13:24 +0100

    now its bootable

--- modules/nixos/luxnix/generic-settings/uids/default.nix
+++ modules/nixos/luxnix/generic-settings/uids/default.nix
@@ -8,6 +8,24 @@ with lib;
 with lib.luxnix; let
   cfg = config.luxnix.generic-settings.uids;
 
+  userConfigs = [
+    {
+      condition = config.roles.lx-anonymizer.enable;
+      user = config.roles.lx-anonymizer.user;
+      uid = cfg.lxAnonymizer;
+    }
+    # Add more user configurations here
+    # {
+    #   condition = config.roles.someOtherRole.enable;
+    #   user = config.roles.someOtherRole.user;
+    #   uid = cfg.someOtherUid;
+    # }
+  ];
+
+  generateUserConfig = userConfig: mkIf userConfig.condition {
+    users.users.${userConfig.user}.uid = userConfig.uid;
+  };
+
 in {
   options.luxnix.generic-settings.uids = { 
     lxAnonymizer = mkOption {
@@ -15,11 +33,14 @@ in {
       default = 731;
       description = "UID for lxAnonymizer";
     };
+    # Add more options here
+    # someOtherUid = mkOption {
+    #   type = types.int;
+    #   default = 732;
+    #   description = "UID for some other user";
+    # };
   };
 
-  config = {
-    users.users.${config.roles.lx-anonymizer.user}.uid = cfg.lxAnonymizer;
-    
-  };
+  config = foldl' (acc: userConfig: acc // generateUserConfig userConfig) {} userConfigs;
 }
 
