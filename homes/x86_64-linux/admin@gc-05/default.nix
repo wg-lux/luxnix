5ae803df024518858268b78bc7746b6f4aec0649
maddonix <tlux14@googlemail.com>
2025-01-16 15:33:25 +0100

    add admin user gc 05

---
+++ homes/x86_64-linux/admin@gc-05/default.nix
@@ -0,0 +1,32 @@
+{pkgs, ...}: {
+    cli.programs.git = {
+      enable = true;
+      userName = "peterkczyk";
+      email = "peter.kowalczyk21@gmail.com";
+      # allowedSigners = "SHA256:LNfWnvEthO0QL8DzUxtxHD4VnLxvCZWFmcDhZodk29o";
+    };
+
+  desktops = {
+    plasma = {
+      enable = true;
+    };
+  };
+
+  services.luxnix = {
+    # syncthing.enable = false;
+  };
+
+  luxnix.generic-settings = {
+    enable = true;
+    configurationPath = "dev/luxnix";
+  };
+
+  roles = {
+    development.enable = true;
+    social.enable = true;
+    gpu.enable = true;
+    video.enable = true;
+  };
+
+  home.stateVersion = "23.11";
+}
