81a7c9e7f74e0e1a167ac91aa80fbc222c30a278
maddonix <tlux14@googlemail.com>
2025-01-29 17:48:15 +0100

    use correct settings now?

--- systems/x86_64-linux/s-02/default.nix
+++ systems/x86_64-linux/s-02/default.nix
@@ -21,28 +21,8 @@
     aglnet.client.enable = true;
     base-server.enable = true;
     endoreg-client.enable = false;
-    postgres.main.authentication = ''
-#type database DBuser address auth-method optional_ident_map
-local sameuser all peer map=superuser_map
-host  all all ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256 map=superuser_map
-host  replication ${config.roles.postgres.main.replUser} ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256
-host  ${config.roles.postgres.main.devUser} ${config.roles.postgres.main.devUser} ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256
-host  all postgres ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256 map=superuser_map
-'';
-  postgres.main.enable = true;
-    postgres.main.identMap = ''
-# ArbitraryMapName systemUser DBUser
-superuser_map      root      postgres
-superuser_map      root      ${config.roles.postgres.main.replUser}
-superuser_map      ${config.user.admin.name}     ${config.user.admin.name}
-superuser_map      ${config.user.admin.name}     postgres
-superuser_map      ${config.user.admin.name}     endoregClient
-superuser_map      postgres  postgres
-
-# Let other names login as themselves
-superuser_map      /^(.*)$   \1
-'';
-  };
+    postgres.main.enable = true;
+    };
 
   services = {
     };
@@ -72,7 +52,27 @@ vault.key = "/etc/secrets/.key";
 
 vault.psk = "/etc/secrets/.psk";
 
-generic-settings.hostPlatform = "x86_64-linux";
+generic-settings.postgres.activeAuthentication = ''
+#type database DBuser address auth-method optional_ident_map
+local sameuser all peer map=superuser_map
+host  all all ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256 map=superuser_map
+host  replication ${config.roles.postgres.main.replUser} ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256
+host  ${config.roles.postgres.main.devUser} ${config.roles.postgres.main.devUser} ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256
+host  all postgres ${config.luxnix.generic-settings.adminVpnIp}/32 scram-sha-256 map=superuser_map
+''; 
+  generic-settings.postgres.identMap = ''
+# ArbitraryMapName systemUser DBUser
+superuser_map      root      postgres
+superuser_map      root      ${config.roles.postgres.main.replUser}
+superuser_map      ${config.user.admin.name}     ${config.user.admin.name}
+superuser_map      ${config.user.admin.name}     postgres
+superuser_map      ${config.user.admin.name}     endoregClient
+superuser_map      postgres  postgres
+
+# Let other names login as themselves
+superuser_map      /^(.*)$   \1
+''; 
+  generic-settings.hostPlatform = "x86_64-linux";
 
 generic-settings.linux.cpuMicrocode = "intel";
 
