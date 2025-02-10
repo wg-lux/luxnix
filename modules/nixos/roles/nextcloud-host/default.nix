ab63f7ee09f1b14f9ebd30af9644c98c67c2e315
maddonix <tlux14@googlemail.com>
2025-02-10 15:45:04 +0100

    fix

--- modules/nixos/roles/nextcloud-host/default.nix
+++ modules/nixos/roles/nextcloud-host/default.nix
@@ -163,7 +163,6 @@ in {
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-NginX-Proxy true;
           proxy_set_header X-Forwarded-Proto http;
-          proxy_pass http://127.0.0.1/; # tailing / is important!
           proxy_set_header Host $host;
           proxy_cache_bypass $http_upgrade;
           proxy_redirect off;
