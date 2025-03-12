05c77386425f61d6b7ac1c564617c3ee88529709
maddonix <tlux14@googlemail.com>
2025-03-12 13:11:26 +0100

    add decryption stick to s-04

--- systems/x86_64-linux/s-04/boot-decryption-config.nix
+++ systems/x86_64-linux/s-04/boot-decryption-config.nix
@@ -1,11 +1,24 @@
 let
-  usb-uuid = "00acbcc0-b74f-4442-9460-b2e2d4e5042d";
+  usb-uuid = "329d74c9-6b32-42d3-b61e-17336e5f97a4";
   usb-mountpoint = "/mnt/usb_key";
-  usb-device = "/dev/disk/by-uuid/00acbcc0-b74f-4442-9460-b2e2d4e5042d";
+  usb-device = "/dev/disk/by-uuid/329d74c9-6b32-42d3-b61e-17336e5f97a4";
 
   bs = 1;
   offset-m = 50;
   offset-b = 52428800;
   keyfile-size = 4096;
 in
-{ }
+{
+  # Ensure necessary kernel modules for USB and LUKS
+  boot.initrd.availableKernelModules = [ "dm-crypt" "sd_mod" "usb_storage" ];
+
+  # 'cryptroot' is defined by disko as the name of the LUKS container.
+  # Use the usb-device as keyFile, with offset and size defined above.
+  boot.initrd.luks.devices."cryptroot" = {
+    keyFile = usb-device;
+    keyFileOffset = offset-b;
+    keyFileSize = keyfile-size;
+    preLVM = true;
+    keyFileTimeout = 10; # if no prompt is displayed, try pressing "Esc"
+  };
+}
