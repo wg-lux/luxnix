63faf3d5ef1636a8ff05bd9e8c6b914bcef2e74f
maddonix <tlux14@googlemail.com>
2025-03-26 12:55:48 +0100

    add mh to dev access

--- systems/x86_64-linux/gs-01/default.nix
+++ systems/x86_64-linux/gs-01/default.nix
@@ -26,6 +26,8 @@
     endoreg-client.enable = true;
     ssh-access.dev-03.enable = true;
     ssh-access.dev-03.idEd25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBJcYjGNIwOUs+KG8TbBxPWtJFEqni0p+1J5Yz++Aos";
+    ssh-access.dev-01.enable = true;
+    ssh-access.dev-01.idEd25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh2Bg+mSSvA80ALScpb81Q9ZaBFdacdxJZtAfZpwYkK";
     ssh-access.dev-04.enable = true;
     ssh-access.dev-04.idEd25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSpoZVcX+K6NdrfqcUVPTU8Ljqlp83YDzzEHjTHU2NO flippos@inexen9";
     };
--- systems/x86_64-linux/s-04/default.nix
+++ systems/x86_64-linux/s-04/default.nix
@@ -23,6 +23,8 @@
     custom-packages.cloud = true;
     custom-packages.enable = true;
     endoreg-client.enable = false;
+    ssh-access.dev-01.enable = true;
+    ssh-access.dev-01.idEd25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh2Bg+mSSvA80ALScpb81Q9ZaBFdacdxJZtAfZpwYkK";
     ssh-access.dev-03.enable = true;
     ssh-access.dev-03.idEd25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBJcYjGNIwOUs+KG8TbBxPWtJFEqni0p+1J5Yz++Aos";
     ssh-access.dev-04.enable = true;
