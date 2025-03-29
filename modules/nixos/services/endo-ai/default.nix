21d05983d04474d06d22bca6034deef80f896ccb
maddonix <tlux14@googlemail.com>
2025-03-29 18:23:18 +0100

    switch to other branch

--- modules/nixos/services/endo-ai/default.nix
+++ modules/nixos/services/endo-ai/default.nix
@@ -32,7 +32,7 @@ with lib.luxnix; let
     cd ${repoDir}
 
     # we can also use specific branches: checkout and pull branch "v0.1.1"
-    # git checkout v0.1.1
+    git checkout minor-fix
     git pull
     exec devenv shell -- run-prod-server
   '';
