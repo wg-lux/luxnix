350c3563a4fb82fa531b0459522ab6393cbb3516
maddonix <tlux14@googlemail.com>
2025-04-05 13:50:12 +0200

    modify udp buffer size

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -16,6 +16,9 @@ in {
   };
 
   config = mkIf cfg.enable {
+
+    boot.kernel.sysctl."net.core.rmem_max" = 7500000;
+    boot.kernel.sysctl."net.core.wmem_max" = 7500000;
     environment.systemPackages = with pkgs;
       [
         efibootmgr
