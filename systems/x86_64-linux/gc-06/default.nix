a07e9e7e149ab507b5d558c8ff29a671e667b4e7
maddonix <tlux14@googlemail.com>
2025-04-08 20:10:40 +0200

    build nix system config

--- systems/x86_64-linux/gc-06/default.nix
+++ systems/x86_64-linux/gc-06/default.nix
@@ -169,6 +169,10 @@ generic-settings.vpnSubnet = "172.16.255.0/24";
 
 gpu-eval.enable = true;
 
+luxnix.generic-settings.linux.rmem-max = 4194304;
+
+luxnix.generic-settings.linux.wmem-max = 4194304;
+
 maintenance.autoUpdates.dates = "09:00";
 
 maintenance.autoUpdates.enable = false;
