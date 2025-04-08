b7d1eb0a8fe831d10332566aeb779347fc72dba3
maddonix <tlux14@googlemail.com>
2025-04-08 20:03:37 +0200

    make rmem and wmem max options to customize this on machines

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -17,8 +17,9 @@ in {
 
   config = mkIf cfg.enable {
 
-    boot.kernel.sysctl."net.core.rmem_max" = 7500000;
-    boot.kernel.sysctl."net.core.wmem_max" = 7500000;
+    boot.kernel.sysctl."net.core.rmem_max" = config.luxnix.generic-settings.linux.rmem_max;
+    boot.kernel.sysctl."net.core.wmem_max" = config.luxnix.generic-settings.linux.wmem_max;
+
     environment.systemPackages = with pkgs;
       [
         efibootmgr
