f63d6d9334b8d9d01dc8451076ec71ee2626fac6
maddonix <tlux14@googlemail.com>
2025-08-03 11:57:17 +0200

    deploy on s-04

--- systems/x86_64-linux/s-04/default.nix
+++ systems/x86_64-linux/s-04/default.nix
@@ -23,7 +23,9 @@
     common.enable = true;
     custom-packages.cloud = true;
     custom-packages.enable = true;
-    endoreg-client.enable = false;
+    endoreg-client.dbApiLocal = true;
+    endoreg-client.enable = true;
+    postgres.default.enable = true;
     ssh-access.dev-01.enable = true;
     ssh-access.dev-01.idEd25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh2Bg+mSSvA80ALScpb81Q9ZaBFdacdxJZtAfZpwYkK";
     ssh-access.dev-03.enable = true;
