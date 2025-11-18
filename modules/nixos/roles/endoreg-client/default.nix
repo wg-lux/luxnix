bc5b460f596215f01716f3c5892822a5a9b2cf1d
maddonix <tlux14@googlemail.com>
2025-11-18 10:29:28 +0100

    update lxAnnotate default branch

--- modules/nixos/roles/endoreg-client/default.nix
+++ modules/nixos/roles/endoreg-client/default.nix
@@ -413,7 +413,7 @@ in
 
             branch = mkOption {
               type = types.str;
-              default = "erc";
+              default = "main";
               description = "Git branch to checkout for lx-annotate.";
             };
 
