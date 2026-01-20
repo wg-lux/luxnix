a3ccab65b86dc92eaa745d474594ac3834519a9c
maxhild <maxhild10@gmail.com>
2026-01-20 23:21:56 +0100

    start file watcher exec

--- systems/x86_64-linux/c-01/default.nix
+++ systems/x86_64-linux/c-01/default.nix
@@ -44,7 +44,7 @@ generic-settings.adminVpnIp = "172.16.255.106";
 
 generic-settings.enable = true;
 
-generic-settings.linux.kernelPackages = pkgs.linuxPackages_6_12;
+generic-settings.linux.kernelPackages = pkgs.linuxPackages_6_6;
 
 generic-settings.network.hosts.gc-01.domains = ["gc-01.intern" "lx-annotate.local"];
 generic-settings.network.hosts.gc-01.ip-vpn = "172.16.255.101";
