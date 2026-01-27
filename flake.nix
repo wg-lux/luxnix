266e8cb4f811977946cdf1fb11cdbca96b28aca6
Thomas <tlux14@googlemail.com>
2026-01-27 14:38:00 +0100

    revert 25.0 to migrate nextcloud to 30

--- flake.nix
+++ flake.nix
@@ -9,10 +9,10 @@
     ];
   };
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     devenv.url = "github:cachix/devenv";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -95,7 +95,7 @@
     nixvim = {
       # url = "github:nix-community/nixvim";
       # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim/nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
