5ffe5fa47146d8ce70eb28d1542f7c4728c95505
Hamzaukw <hamza.ukw@gmail.com>
2026-05-05 14:25:42 +0200

    addition:  adding url for checkpoints

--- modules/nixos/roles/endoreg-client/default.nix
+++ modules/nixos/roles/endoreg-client/default.nix
@@ -277,6 +277,8 @@ in
         # optional future improvements
         # source = cfg.repository (if needed)
         # debug.enable = false
+        runtime.backboneCheckpointUrl =
+         "https://drive.google.com/uc?export=download&id=1TvliEJ5JTQddIE3kNiGMQzWIe9Cq_7mx";
       };
 
       services.luxnix.endoAi = {
