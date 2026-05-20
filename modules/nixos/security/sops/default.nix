09f9a0c903b9fe8ffb8b25335e45bb7fa323989f
maxhild <maxhild10@gmail.com>
2026-05-20 17:27:44 +0200

    Disable undeployed SOPS secrets

--- modules/nixos/security/sops/default.nix
+++ modules/nixos/security/sops/default.nix
@@ -19,21 +19,22 @@ in {
       defaultSopsFormat = "yaml";
       age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
-      secrets = {
-        client_user_password_hash = {
-          path = "/etc/secrets/vault/SCRT_client_user_password_hash";
-          owner = "root";
-          group = "root";
-          mode = "0400";
-          neededForUsers = true;
-        };
+      # TODO: Deploy SOPS keys and enable
+      # secrets = {
+      #   client_user_password_hash = {
+      #     path = "/etc/secrets/vault/SCRT_client_user_password_hash";
+      #     owner = "root";
+      #     group = "root";
+      #     mode = "0400";
+      #     neededForUsers = true;
+      #   };
 
-        nextcloud_host_password = mkIf config.roles.nextcloudHost.enable {
-          path = "/etc/secrets/vault/SCRT_roles_system_password_nextcloud_host_password";
-          owner = "root";
-          group = "root";
-          mode = "0400";
-        };
+      #   nextcloud_host_password = mkIf config.roles.nextcloudHost.enable {
+      #     path = "/etc/secrets/vault/SCRT_roles_system_password_nextcloud_host_password";
+      #     owner = "root";
+      #     group = "root";
+      #     mode = "0400";
+      #   };
       };
     };
   };
