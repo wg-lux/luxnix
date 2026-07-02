e009d4dc5ffc8e32422bfcecbf5fee20553c1c95
maxhild <maxhild10@gmail.com>
2026-07-02 12:56:24 +0200

    python huggingface no 3.14

--- modules/nixos/services/glm-5-2/default.nix
+++ modules/nixos/services/glm-5-2/default.nix
@@ -110,7 +110,7 @@ in
 
     package = mkPackageOpt pkgs.llama-cpp "llama.cpp package used for `llama-server`.";
 
-    huggingFaceHubPackage = mkPackageOpt pkgs.python314Packages.huggingface-hub "Python package providing the `hf` CLI used by the download unit.";
+    huggingFaceHubPackage = mkPackageOpt pkgs.python313Packages.huggingface-hub "Python package providing the `hf` CLI used by the download unit.";
 
     user = mkOpt types.str "glm-5-2" "System user used to run GLM-5.2 services.";
 
