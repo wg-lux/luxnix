cc5d713840db22c22388da5b0a8ba5253f544ae0
Hamzaukw <hamza.ukw@gmail.com>
2026-08-17 15:57:12 +0200

    avoid duplication...as standard proxy headers are supplied by the NixOS

--- modules/nixos/services/lx-annotate-local/config.nix
+++ modules/nixos/services/lx-annotate-local/config.nix
@@ -83,11 +83,13 @@ let
     send_timeout 21600s;
 
     # Explicit reverse-proxy contract used by Django.
-    proxy_set_header Host $host;
-    proxy_set_header X-Forwarded-Host $host;
-    proxy_set_header X-Forwarded-Proto $scheme;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Real-IP $remote_addr;
+    # proxy_set_header Host $host;
+    # proxy_set_header X-Forwarded-Host $host;
+    # proxy_set_header X-Forwarded-Proto $scheme;
+    # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    # proxy_set_header X-Real-IP $remote_addr;
+    # Standard proxy headers are supplied by the NixOS recommended, so commented
+
 
     # Never trust an incoming value for this header. Replace it with the
     # result of Nginx client-certificate verification.
